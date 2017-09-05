972b11a67fda51310b940b85fe6ac1f33a737f0a
WiseBell <neoswirl@gmail.com>
2017-09-05 21:40:01 +0900

    config.read_encrypted_secrets 설정 변

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,6 @@ set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
-# set :deploy_to,       '/var/www/apps/minerva'
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"

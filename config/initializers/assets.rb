d1fa3f1eaef94621e78599bff1162d264414a46a
WiseBell <neoswirl@gmail.com>
2017-09-11 18:50:59 +0900

    assetpipelin에 자원 추가

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,7 +13,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
-Rails.application.config.assets.precompile += %w( main.css )
+Rails.application.config.assets.precompile += %w( main.css font-awesome.min.css )
 Rails.application.config.assets.precompile += %w( jquery.min.js skel.min.js util.js ie/respond.min.js main.js )
 Rails.application.config.assets.precompile += %w( fontawesome-webfont.woff2 fontawesome-webfont.woff fontawesome-webfont.ttf )
 
